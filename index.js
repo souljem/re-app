8700801381f44cb22a2aacf65f117912e4e134f9
souljem <souljem@gmail.com>
2017-09-14 21:31:39 -0700

    testing node

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-function displayName () {
+var http = require('http');
 
-	 var name = document.getElementById("name");
+http.createServer(function (req, res) {
 
-	 document.getElementById("displayName") = name.value;
-}
+	res.writeHead(200, {"Content-Type": "text/plain"})
+	res.end("hello World\n")
+}).listen(process.env.PORT)
+// var { Client } = require('pg');
+
+// var client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
+
+
+
+
+// function displayName () {
+
+// 	 var name = document.getElementById("name");
+
+// 	 document.getElementById("displayName") = name.value;
+// }
